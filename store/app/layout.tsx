3037171c9c3ab7510937b15b4e56acbc3f2f1158
scarlet <scarletvf47@gmail.com>
2024-06-05 18:08:42 -0400

    Main configuration and featured products.

---
+++ store/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Urbanist } from "next/font/google";
+
+import Footer from "@/components/footer";
+import Navbar from "@/components/navbar";
+
+import "./globals.css";
+
+const urbanist = Urbanist({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Store",
+  description: "Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={urbanist.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ store/components/ui/product-card.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { Expand, ShoppingCart } from "lucide-react";
+import Image from "next/image";
+
+import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
+import { Product } from "@/types";
+
+interface ProductCardProps {
+  data: Product;
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  return (
+    <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* Images and Actions */}
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          className="aspect-square object-cover rounded-md"
+          src={data?.images?.[0]?.url}
+          alt="Image"
+          fill
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton icon={<Expand className="text-gray-600 h-5 w-5" />} />
+            <IconButton icon={<ShoppingCart className="text-gray-600 h-5 w-5" />} />
+          </div>
+        </div>
+      </div>
+      {/* Description */}
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Price */}
+      <div className="flex justify-between items-center">
+        <Currency value={data?.price} />
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
