aa34a7bcecf4f2e1711abb9b192aefafed234d07
scarlet <scarletvf47@gmail.com>
2024-06-08 17:56:36 -0400

    fix bugs

--- store/components/main-nav.tsx
+++ store/components/main-nav.tsx
@@ -14,7 +14,7 @@ const MainNav: React.FC<MainNavProps> = ({ data }) => {
   const pathname = usePathname();
 
   const routes = data.map((route) => ({
-    href: "/category/${route.id}",
+    href: `/category/${route.id}`,
     label: route.name,
     active: pathname === `/category/${route.id}`,
   }));
