6be224859d97cb4eb22b36297f75424e52d14aad
scarlet <scarletvf47@gmail.com>
2024-06-09 16:49:25 -0400

    changes

--- store/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ store/app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -15,18 +15,18 @@ interface MobileFiltersProps {
 }
 
 const MobileFilters: React.FC<MobileFiltersProps> = ({ sizes, colors }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [open, setOpen] = useState(false);
 
-  const onOpen = () => setIsOpen(true);
-  const handleOnClose = () => setIsOpen(true);
+  const onOpen = () => setOpen(true);
+  const onClose = () => setOpen(false);
 
   return (
     <>
       <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
         Filters
-        <Plus className="h-5 w-5" />
+        <Plus size={20} />
       </Button>
-      <Dialog open={isOpen} as="div" className="relative z-40 lg:hidden" onClose={handleOnClose}>
+      <Dialog open={open} as="div" className="relative z-40 lg:hidden" onClose={onClose}>
         {/* Background */}
         <div className="fixed inset-0 bg-black bg-opacity-25" />
         {/* Dialog Position} */}
@@ -34,7 +34,9 @@ const MobileFilters: React.FC<MobileFiltersProps> = ({ sizes, colors }) => {
           <DialogPanel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
             {/* Close Button */}
             <div className="flex items-center justify-end px-4">
-              <IconButton icon={<X size={15} />} onClick={handleOnClose} />
+              <IconButton onClick={onClose}>
+                <X size={15} />
+              </IconButton>
             </div>
             {/* Render the filters */}
             <div className="p-4">
